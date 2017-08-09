3e6469c272f4b17b4f1663b7c28f6c5976619a96
liujiang(刘江).apabi <liu.jiang@HOLD.FOUNDER.COM>
2017-08-09 14:55:13 +0800

    config default file

--- src/main/java/com/oauth2client/controller/HomeController.java
+++ src/main/java/com/oauth2client/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.oauth2client.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
 
 }
--- src/main/java/com/oauth2client/service/impl/UserServiceImpl.java
+++ src/main/java/com/oauth2client/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.oauth2client.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.oauth2client.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 }
